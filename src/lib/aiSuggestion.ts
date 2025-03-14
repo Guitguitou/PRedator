b8be558a7c4961f469682fcc4d00c7ec6b0fdfec
guillaume imary <guillaume.imary@gmail.com>
2025-03-14 18:22:27 +0100

    ajoute un dossier public

--- src/lib/aiSuggestion.ts
+++ src/lib/aiSuggestion.ts
@@ -4,24 +4,27 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // Assurez-vous que votre clé API est définie dans les variables d'environnement
 });
 
+console.log("OPENAI_API_KEY", "j'ai ma clé api");
+
 export async function aiSuggestion(comment: string, diff: string, filepath: string) {
   // Créer un thread
   const createThreadResponse = await openai.beta.threads.create();
+  console.log("createThreadResponse", createThreadResponse);
   const threadId = createThreadResponse.id;
-
+  console.log("threadId", threadId);
   // Ajouter un message au thread
   await openai.beta.threads.messages.create(threadId, {
     role: "user",
     content: `Voici le commentaire: ${comment} Voici le diff: ${diff} Voici le fichier: ${filepath}`
-  });
-
+    });
+  console.log("message créé");
   // Exécuter le thread avec l'ID de l'assistant
   const runThreadResponse = await openai.beta.threads.runs.create(threadId, {
     assistant_id: "asst_B6GIc92FAGYDf3xBcr2CiwX0"
   });
-
+  console.log("runThreadResponse", runThreadResponse);
   const runId = runThreadResponse.id;
-
+  console.log("runId", runId);
   // Attendre que le run soit terminé
   let status = "running";
   while (status !== "completed") {
